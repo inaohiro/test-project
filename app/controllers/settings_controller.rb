512d5954bb6423948f2eadb4aab5bc8b3a74156c
Naohiro Iwamoto <naohiro-iwamoto@gmo.jp>
2018-07-21 20:37:33 +0900

    small refactoring

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,8 +1,6 @@
 class SettingsController < ApplicationController
   def settings
-    user = Token.find_by(id: session[:user_id])
-
-    @item = user.items.build if logged_in?
+    @item = current_user.items.build if logged_in?
   end
 
   def tutorial
