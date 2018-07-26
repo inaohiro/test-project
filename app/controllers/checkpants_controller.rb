de093754c0d53f27c5236489245f61c2003cc573
Naohiro Iwamoto <naohiro-iwamoto@gmo.jp>
2018-07-26 15:22:19 +0900

    notify

--- app/controllers/checkpants_controller.rb
+++ app/controllers/checkpants_controller.rb
@@ -2,19 +2,26 @@ class CheckpantsController < ApplicationController
 
     def getFewpantsToken
 
-        data=[]
+        # data=[]
+        # Token.all.each do |user|
+        #     if user.items.length > 0
+        #         if user.items.first.current < 3
+        #             unless user.notification_token.empty?
+        #             #if user.notification_token.length > 0
+        #                 data.push(user.notification_token)
+        #             end
+        #         end
+        #     end
+        # end
+        # p data
+        # data
+
         Token.all.each do |user|
-            if user.items.length > 0
-                if user.items.first.current < 3
-                    unless user.notification_token.empty?
-                    #if user.notification_token.length > 0
-                        data.push(user.notification_token)
-                    end
-                end
-            end
+          if user.notification
+            Notification.new.notify( user.notification.notification_token )
+            sleep(0.02)
+          end
         end
-        p data
-        data
 
     end
 
