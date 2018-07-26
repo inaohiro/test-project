841c0b1f6f6decb68e7acebae7c380689247c621
Naohiro Iwamoto <naohiro-iwamoto@gmo.jp>
2018-07-26 14:44:24 +0900

    change http request sender to httpclient

--- Gemfile
+++ Gemfile
@@ -17,6 +17,8 @@ gem 'bcrypt', '~> 3.1.7'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
 
+gem 'httpclient'
+
 
 group :development do
   # Access an interactive console on exception pages or by calling 'console' anywhere in the code.
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -5,17 +5,21 @@ class Notification < ApplicationRecord
     if ENV["SERVER_ID"]
       serverid = ENV["SERVER_ID"]
 
-      header1 = "Authorization: key=#{serverid}"
-      header2 = "Content-Type: application/json"
-      data = {
-        to: notification_token,
-        notification: {
-          body: "Hi, I\'m Pants Manager"
-        }
-      }
+      # header1 = "Authorization: key=#{serverid}"
+      # header2 = "Content-Type: application/json"
+      # data = {
+      #   to: notification_token,
+      #   notification: {
+      #     body: "Hi, I\'m Pants Manager"
+      #   }
+      # }
 
-      system("curl -X POST --header #{header1} --header #{header2} -d #{data.to_json} https://fcm.googleapis.com/fcm")
+      # system("curl -X POST --header #{header1} --header #{header2} -d #{data.to_json} https://fcm.googleapis.com/fcm")
+
+      client = HTTPClient.new
+      client.post("https://fcm.googleapis.com/fcm", header: [["Authorization", "key=#{serverid}"], ["Content-Type", "application/json"]], body: data.to_json)
     else
+      p "hoge"
     end
   end
 end
