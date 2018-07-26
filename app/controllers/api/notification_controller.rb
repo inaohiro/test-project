1d204382b9f38cef52342baffe52be2a96f727be
kihoair <kihoair@gmail.com>
2018-07-26 10:36:20 +0900

    api: notification complete!

--- app/controllers/api/notification_controller.rb
+++ app/controllers/api/notification_controller.rb
@@ -1,2 +1,10 @@
 class Api::NotificationController < ApplicationController
+
+    def push
+
+        notification = current_user.build_notification
+        notification.update_attributes({notification_token:  params[:notification]})
+
+    end
+
 end
